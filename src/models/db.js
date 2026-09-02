05fe34fa30138af636557f3bf46563852bcef695
tedayley <54820117+tedayley@users.noreply.github.com>
2026-09-02 10:17:09 -0400

    ssl no longer set to true, but instead authorization ignored.

--- src/models/db.js
+++ src/models/db.js
@@ -2,7 +2,9 @@ import { Pool } from "pg";
 
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 let db = null;
